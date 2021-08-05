748843100bc7bcb752a212f721967099d7d9c47d
MarahQ <mqarawaez@gmail.com>
2021-08-05 12:44:19 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -1,86 +1,86 @@
-'use strict'
-let userName = prompt("Hey i am the web owner Marah and you?")
+'use strict';
+let userName = prompt('Hey i am the web owner Marah and you?');
 console.log(userName);
-alert("It's a pleasure to see you here" + userName)
-alert("ok we well play a nice game after you read my bigrapgy I'll ask you qustions to make sure your fully understanding who i am")
+alert('It\'s a pleasure to see you here' + userName);
+alert('ok we well play a nice game after you read my bigrapgy I\'ll ask you qustions to make sure your fully understanding who i am');
 let correct = 0;
 function Q1() {
 
   let firstQ = prompt('Can i give you a health consltant session?').toLowerCase();
   switch (firstQ) {
-    case 'yes': case 'y':
-      //console.log('Absolutly thats one of the services i could afford it');
-      alert('Absolutly thats one of the services i could afford it');
-      correct++;
-      break;
-    case 'no': case 'n':
-      //console.log("Read the biography carfully");
-      alert('Read the biography carfully')
-      break;
-    default:
-      //console.log('you can make a tour your in our web');
-      alert("you can make a your in our web")
-      break;
+  case 'yes': case 'y':
+    //console.log('Absolutly thats one of the services i could afford it');
+    alert('Absolutly thats one of the services i could afford it');
+    correct++;
+    break;
+  case 'no': case 'n':
+    //console.log("Read the biography carfully");
+    alert('Read the biography carfully');
+    break;
+  default:
+    //console.log('you can make a tour your in our web');
+    alert('you can make a your in our web');
+    break;
   }
 }
 Q1();
 
 function Q2() {
-  let SecQ = prompt('Can i write you a diet plan?').toUpperCase()
+  let SecQ = prompt('Can i write you a diet plan?').toUpperCase();
   switch (SecQ) {
-    case 'YES': case 'Y':
-      //console.log('Thats one of my strong skills');
-      alert('Thats one of my strong skills');
-      correct++;
-      break;
-    case 'NO': case 'N':
-      //console.log('the first words were i am a dietitian');
-      alert('the first words were i am a dietitian')
-      break;
-    default:
-      //console.log('you can make a your in our web');
-      alert("you can make a your in our web")
-      break;
+  case 'YES': case 'Y':
+    //console.log('Thats one of my strong skills');
+    alert('Thats one of my strong skills');
+    correct++;
+    break;
+  case 'NO': case 'N':
+    //console.log('the first words were i am a dietitian');
+    alert('the first words were i am a dietitian');
+    break;
+  default:
+    //console.log('you can make a your in our web');
+    alert('you can make a your in our web');
+    break;
   }
 }
 Q2();
 
 function Q3() {
-  let ThirdQ = prompt("I've graduted from certified credentials Unevisity?").toLowerCase();
+  let ThirdQ = prompt('I\'ve graduted from certified credentials Unevisity?').toLowerCase();
   switch (ThirdQ) {
-    case 'yes': case 'y':
-      //console.log("It's UOP(Unevisity Of Petra)");
-      alert("It's UOP(Unevisity Of Petra)");
-      correct++;
-      break;
-    case 'no': case 'n':
-      //console.log('You need to be more focused');
-      alert('You need to be more focused')
-      break;
-    default:
-      //console.log('you can make a your in our web');
-      alert("you can make a your in our web")
-      break;
+  case 'yes': case 'y':
+    //console.log("It's UOP(Unevisity Of Petra)");
+    alert('It\'s UOP(Unevisity Of Petra)');
+    correct++;
+    break;
+  case 'no': case 'n':
+    //console.log('You need to be more focused');
+    alert('You need to be more focused');
+    break;
+  default:
+    //console.log('you can make a your in our web');
+    alert('you can make a your in our web');
+    break;
   }
 }
 Q3();
 
 function Q4() {
-  let FourthQ = prompt('My passion is in health awarness & media?').toUpperCase()
+  let FourthQ = prompt('My passion is in health awarness & media?').toUpperCase();
   switch (FourthQ) {
-    case 'YES': case 'Y':
-      console.log('yes');
-      alert('Thats why i am learned how to deal with social media platforms');
-      correct++;
-      break;
-    case 'NO': case 'N':
-      //console.log('I think you dont like the idea of reading');
-      alert('I think you dont like the idea of reading')
-      break;
-    default:
-      //console.log('you can make a your in our web');
-      alert("you can make a your in our web")
-      break;
+  case 'YES': case 'Y':
+    console.log('yes');
+    alert('Thats why i am learned how to deal with social media platforms');
+    correct++;
+    break;
+  case 'NO': case 'N':
+    //console.log('I think you dont like the idea of reading');
+    alert('I think you dont like the idea of reading');
+    break;
+  default:
+    //console.log('you can make a your in our web');
+    alert('you can make a your in our web');
+    break;
   }
 }
 Q4();
@@ -88,40 +88,40 @@ Q4();
 function Q5() {
   let FifthQ = prompt('Do you think i love sport?').toLowerCase();
   switch (FifthQ) {
-    case 'yes': case 'y':
-      //console.log('Kickboxing is my favorit one');
-      alert("Kickboxing is my favorit one");
-      correct++;
-      break;
-    case 'no': case 'n':
-      //console.log('You should answer it logically');
-      alert('You should answer it logically')
-      break;
-    default:
-      //console.log('you can make a your in our web');
-      alert("you can make a your in our web")
-      break;
+  case 'yes': case 'y':
+    //console.log('Kickboxing is my favorit one');
+    alert('Kickboxing is my favorit one');
+    correct++;
+    break;
+  case 'no': case 'n':
+    //console.log('You should answer it logically');
+    alert('You should answer it logically');
+    break;
+  default:
+    //console.log('you can make a your in our web');
+    alert('you can make a your in our web');
+    break;
   }
-  alert("Since i had tell you a lot information about me with mentioning histories i think you can answer the next qustion")
+  alert('Since i had tell you a lot information about me with mentioning histories i think you can answer the next qustion');
 }
 Q5();
 
 function Q6() {
-  let SixthQ = prompt("How old i am?")
-  let SQ = Number(SixthQ)
+  let SixthQ = prompt('How old i am?');
+  let SQ = Number(SixthQ);
   let answer6 = 24;
 
-  for (var i = 0; i < 4; i++)
+  for (let i = 0; i < 3; i++)
     if (SQ === answer6) {
-      alert('Correct')
+      alert('Correct');
       correct++;
       break;
     }
     else if (SQ < answer6) {
-      SQ = parseInt(prompt('Too low, you have 4 attempts only'))
+      SQ = parseInt(prompt('Too low, you have 4 attempts only'));
     }
     else if (SQ > answer6) {
-      SQ = parseInt(prompt('Too high, you have 4 attempts only'))
+      SQ = parseInt(prompt('Too high, you have 4 attempts only'));
     }
   alert('I am 24 years old');
 }
@@ -137,8 +137,9 @@ function Q7() {
       correct++;
       break;
     } else {
-      alert("Review my following list again");
+      alert('Review my following list again');
     }
+    alert('The right answers were \'WHO\', \'MOH\', \'FAO\', \'UNHR\', \'NHCS\', \'Academy of nutrition and dietetics\', \'USDA\', \'FDA\'')
     alert('Youre score is ' + correct + '/7');
   }
 }
